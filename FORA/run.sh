a859a716e52154675aebfe7b31f23c55bfb01390
moon-Light404 <1908328781@qq.com>
2024-11-26 01:18:22 +0800

    ss

--- FORA/run.sh
+++ FORA/run.sh
@@ -17,10 +17,9 @@
 
 # python main.py --iteration 30000 --layer_id 2 --dataset_num 5000 --dlr 3e-4 --a 0.6 --coral
 
-python main.py --iteration 15000 --layer_id 2 --dataset_num 2500 --dlr 3e-4 --a 0.7 --coral
-
 # python main.py --iteration 15000 --layer_id 2 --dataset_num 5000 --dlr 3e-4 --a 0.2 --mkkd
 
+python main.py --iteration 40000 --layer_id 2 --dataset_num 3000 --dlr 3e-4 --a 0.8 --coral
 
-
+  
 
